88f2385aedb7dddce39b2713c06a64ae2642b3dc
Gustavo França <gstvz.franca@gmail.com>
2023-11-08 19:47:11 -0300

    feat: created CustomerVO

---
+++ src/java/VO/CustomerVO.java
@@ -0,0 +1,40 @@
+package VO;
+
+public class CustomerVO {
+    private int id;
+    private String name;
+    private String cpf;
+    private String phone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
